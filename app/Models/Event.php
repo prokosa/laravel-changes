383d1406b5080eefa1af3e07e7256d6d5c465a81
vagrant <vagrant@project>
2016-09-08 14:56:31 +0000

    commit before create controllers

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
-    //
+    public function performers()
+    {
+        return $this->belongsToMany('App\Models\Performer', 'performer_event', 'performer_id', 'event_id');
+    }
 }
--- app/Models/Performer.php
+++ app/Models/Performer.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Performer extends Model
 {
-    //
+    public function events()
+    {
+        return $this->belongsToMany('App\Models\Event', 'performer_event', 'performer_id', 'event_id');
+    }
 }
